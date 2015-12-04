7f6f23fa88701092fc96a650debf1026de7e1f05
luhai <18221536574@163.com>
2015-12-04 13:26:37 +0800

    Update YZprogressHUB.h
    add load gif method

--- src/YZprogressHUB.h
+++ src/YZprogressHUB.h
@@ -9,16 +9,13 @@
 #import <UIKit/UIKit.h>
 
 @interface YZprogressHUB : UIView
-@property (nonatomic) UIActivityIndicatorView *acticityIndicator;
-@property (nonatomic) UILabel *textLabel;
-@property (nonatomic) UIView *hubView;
 
 //property setters
 - (void)setColor:(UIColor *)backgroundColor;
-
+- (void)setText:(NSString *)text;
 //pubic methods
 + (void)hideHUDForView:(UIView *)view;
 + (void)hideHUDForView:(UIView *)view afterDelay:(NSTimeInterval)delay;
 + (instancetype)showHUDAddedTo:(UIView *)view animated:(BOOL)animated;
-
++ (instancetype)showHUDAddedTo:(UIView *)view withGifViewFrame:(CGRect)frame andGifData:(NSData *)data animated:(BOOL)animated;
 @end
