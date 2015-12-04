50aa0c89cbad4ff6847933058b5c3055a068b374
luhai <18221536574@163.com>
2015-12-04 11:21:35 +0800

    Update YZprogressHUB.h
    add hideHUDForView, move property

--- src/YZprogressHUB.h
+++ src/YZprogressHUB.h
@@ -9,7 +9,15 @@
 #import <UIKit/UIKit.h>
 
 @interface YZprogressHUB : UIView
+@property (nonatomic) UIActivityIndicatorView *acticityIndicator;
+@property (nonatomic) UILabel *textLabel;
+@property (nonatomic) UIView *hubView;
+
+//property setters
 - (void)setColor:(UIColor *)backgroundColor;
+
+//pubic methods
++ (void)hideHUDForView:(UIView *)view;
 + (void)hideHUDForView:(UIView *)view afterDelay:(NSTimeInterval)delay;
 + (instancetype)showHUDAddedTo:(UIView *)view animated:(BOOL)animated;
 
